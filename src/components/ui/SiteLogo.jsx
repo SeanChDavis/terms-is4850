4f4508953fe8deaafce9a572655686d2aabd0ac9
SeanChDavis <seanchdavis@gmail.com>
2025-03-31 11:56:21 -0500

    add new files

---
+++ src/components/ui/SiteLogo.jsx
@@ -0,0 +1,16 @@
+import LogoColor from '../../assets/terms-logo-color.svg';
+import LogoWhite from '../../assets/terms-logo-white.svg';
+
+const SiteLogo = ({ variant = 'color', className = '' }) => {
+    const logoSrc = variant === 'white' ? LogoWhite : LogoColor;
+
+    return (
+        <img
+            src={logoSrc}
+            alt="TERMS Logo"
+            className={`h-9 w-auto ${className}`}
+        />
+    );
+};
+
+export default SiteLogo;
