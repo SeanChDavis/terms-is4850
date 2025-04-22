1f939b2edc697e6d70c66bd2726c3eaf19e4e6b4
SeanChDavis <seanchdavis@gmail.com>
2025-04-22 10:23:42 -0500

    fix dialog width on small screens #18

--- src/pages/manager/Announcements.jsx
+++ src/pages/manager/Announcements.jsx
@@ -372,7 +372,7 @@ export default function ManagerAnnouncements() {
                                         className="flex min-h-full items-end justify-center p-4 sm:items-center sm:p-0">
                                         <DialogPanel
                                             transition
-                                            className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                                            className="w-full relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
                                         >
                                             <div>
                                                 <DialogTitle
--- src/pages/manager/User.jsx
+++ src/pages/manager/User.jsx
@@ -214,7 +214,7 @@ export default function ManagerUserView() {
                                     <div
                                         className="flex min-h-full items-end justify-center p-4 sm:items-center sm:p-0">
                                         <DialogPanel
-                                            className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95">
+                                            className="w-full relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95">
                                             <div>
                                                 <DialogTitle
                                                     as="h3"
