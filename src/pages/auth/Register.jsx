ccb8311f59dbb46222358c7c481420dd5d1fa899
RumaboyLotti <oladimeji.bada18@gmail.com>
2025-04-28 23:08:06 -0500

    renamed flag to managerApproved #13 '

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -28,7 +28,7 @@ const Register = () => {
             await createUserDocument(uid, {
                 email,
                 role: 'employee',
-                registerFlag: "False"
+                managerApproved: "False"
             });
 
             addToast({  // Add this toast
@@ -71,7 +71,7 @@ const Register = () => {
             await createUserDocument(uid, {
                 email,
                 role: 'employee', // Default all new users to employee
-                registerFlag: "False",
+                managerApproved: "False",
             });
 
             addToast({  // Add this toast
