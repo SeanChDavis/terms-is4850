12b23453b22681ac60587d3c1efb0faaecdaf674
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2025-05-13 22:25:57 -0500

    Update firebase-config.js
    removed credentials

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,14 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCLl8sH6VB0MVUBj5JLi_82ljcUJPW6UF4",
-    authDomain: "terms-prod.firebaseapp.com",
-    projectId: "terms-prod",
-    storageBucket: "terms-prod.firebasestorage.app",
-    messagingSenderId: "35489786391",
-    appId: "1:35489786391:web:fa5684b298f99b0af9ca52"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
