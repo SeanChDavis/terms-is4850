b431f028ad17d3c8538a6513da2b7536768f5a92
SeanChDavis <seanchdavis@gmail.com>
2025-05-19 08:05:51 -0500

    remove credentials and mark retired

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,12 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLl8sH6VB0MVUBj5JLi_82ljcUJPW6UF4",
-    authDomain: "terms-prod.firebaseapp.com",
-    projectId: "terms-prod",
-    storageBucket: "terms-prod.firebasestorage.app",
-    messagingSenderId: "35489786391",
-    appId: "1:35489786391:web:fa5684b298f99b0af9ca52"
+    // project retired
 };
 
 const app = initializeApp(firebaseConfig);
